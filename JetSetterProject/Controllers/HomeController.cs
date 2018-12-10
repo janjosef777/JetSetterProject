46824e22e70608428a877be67d54efaec21c2778
A01073141 <A01073141@DTC01L0382-24>
2018-12-10 12:38:04 -0800

    Added [Authorize] so need to be logged to acces to About

--- JetSetterProject/Controllers/HomeController.cs
+++ JetSetterProject/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JetSetterProject.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JetSetterProject.Controllers
 {
@@ -14,7 +15,7 @@ namespace JetSetterProject.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";

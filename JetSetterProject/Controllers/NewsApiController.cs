fa89942b6031db15e265d73229a6a37f2d7db7f8
Lawrence Lu <lawrencelu18@gmail.com>
2019-02-28 10:46:03 -0800

    Added News API and controller

---
+++ JetSetterProject/Controllers/NewsApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using JetSetterProject.ViewModels;
+
+namespace JetSetterProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsApiController : ControllerBase
+    {
+        private readonly string url = "https://newsapi.org/v2/top-headlines?" +
+          "country=us&" +
+          "apiKey=772941f406474aa497dc367c0a27288f";
+
+        [HttpGet]
+        public dynamic GetAll()
+        {
+            WebClient webClient = new WebClient();
+            NewsVM result = JsonConvert.DeserializeObject<NewsVM>(webClient.DownloadString(url));
+            return result;
+        }
+
+    }
+}
---
+++ JetSetterProject/ViewModels/ArticlesVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JetSetterProject.ViewModels
+{
+    public class ArticlesVM
+    {
+        public Source Source { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public string URLToImage { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Content { get; set; }
+    }
+    public class Source
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}

6d13c4935bc1b442b03acd04b12cfe74999a414c
A01083930 <A01083930@DTC01L0360-05>
2019-03-05 11:20:56 -0800

    added facebook and google logins

---
+++ JetSetterProject/Controllers/NewsApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using JetSetterProject.ViewModels;
+
+namespace JetSetterProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsApiController : ControllerBase
+    {
+        private readonly string url = "https://newsapi.org/v2/everything?" +
+          "q=Apple&" +
+          "from=2019-03-05&" +
+          "sortBy=popularity&" +
+          "apiKey=772941f406474aa497dc367c0a27288f";
+
+        [HttpGet]
+        public NewsVM GetAll()
+        {
+            WebClient webClient = new WebClient();
+            NewsVM result = JsonConvert.DeserializeObject<NewsVM>(webClient.DownloadString(url));
+            return result;
+        }
+
+    }
+}

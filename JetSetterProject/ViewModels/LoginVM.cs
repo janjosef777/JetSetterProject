cc91eb828aa715aff1411aea343425f52dd3ffad
Lawrence Lu <lawrencelu18@gmail.com>
2019-03-12 18:21:43 -0700

    Added Login Controller Merged Register & Login page into one

---
+++ JetSetterProject/ViewModels/LoginVM.cs
@@ -0,0 +1,25 @@
+﻿using jetsetterProj.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JetSetterProject.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
